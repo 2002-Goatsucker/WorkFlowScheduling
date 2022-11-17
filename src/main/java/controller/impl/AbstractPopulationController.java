3a239ca30b1b4caa2821c37fe6c9dbc5f6430251
LinJie <1249706509@qq.com>
2022-11-17 19:25:46 +0800

    add controller

--- src/main/java/controller/impl/AbstractPopulationController.java
+++ src/main/java/controller/impl/AbstractPopulationController.java
@@ -1,9 +1,15 @@
 package controller.impl;
 
+import utils.ConfigUtils;
+
+import java.util.ResourceBundle;
+
 public abstract class AbstractPopulationController {
     private int size;
+    private double mutation;
 
     public void initiatePopulation(){
+        size= Integer.parseInt(ConfigUtils.get("evolution.population.size"));
 
     }
 
