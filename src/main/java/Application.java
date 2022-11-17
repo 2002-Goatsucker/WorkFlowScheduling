02c786fd871bc5eec0af4ece3d79b9b18ed96e2c
LinJie <1249706509@qq.com>
2022-11-17 23:27:38 +0800

    finish PopulationController

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,12 @@
+import controller.PopulationController;
+import controller.impl.NSGAIIPopulationController;
+import service.io.Input;
+import service.io.impl.ConsoleInputImpl;
+
 public class Application {
     public static void main(String[] args) {
-
+        Input input=new ConsoleInputImpl();
+        PopulationController controller=new NSGAIIPopulationController();
+        controller.iterate();
     }
 }
