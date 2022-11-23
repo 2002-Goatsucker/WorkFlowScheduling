068c0b7207fa20f07d41bdfcde120d4f024d3c15
Pilusimida <12011415@mail.sustech.edu.cn>
2022-11-23 19:09:46 +0800

    finish the HEFT

--- src/main/java/service/algorithm/HEFT.java
+++ src/main/java/service/algorithm/HEFT.java
@@ -85,8 +85,10 @@ public class HEFT {
 
         return null;
         }
-        Chromosome chromosome = new Chromosome(order, task2ins, ins2type);
+        c.setTask(order);
+        c.setTask2ins(task2ins);
+        c.setIns2type(ins2type);
 
-        return chromosome;
+        return c;
     }
 }
